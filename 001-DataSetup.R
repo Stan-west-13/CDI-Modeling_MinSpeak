fe840bb87d9b391bfba8e73961e5b99f98952573
Swest13 <swest19@lsu.edu>
2026-01-14 15:01:32 -0600

    Annotate

--- 001-DataSetup.R
+++ 001-DataSetup.R
@@ -1,12 +1,25 @@
 library(tidyverse)
 library(readxl)
 
-## Load in Mininmally Speaking and long-formatted CDI data
+## Load in Minimally Speaking and long-formatted CDI data
 min_speak_samp <- read_rds("data/CDI_multiple_timpoints.rds")
 load("data/ASD_all_GM.Rdata")
-cdi <- read_rds("data/cdi-metadata 1 (1).rds")
-## Merge in Item-wise data
+cdi <- read_rds("data/cdi-metadata.rds")
+
+## Making sure WG and WS have the same CDI-compatible lemmas
+sum(unique(ASD_all$CDI_Metadata_compatible[ASD_all$form == "WG"]) %in% unique(ASD_all$CDI_Metadata_compatible[ASD_all$form == "WS"]) )
+## 395 words from WG overlap with WS, so using the CDI_Metadata_compatible column to join in CDI word IDs is feasible. 
+
+## Merge in Item-wise data and CDI word IDs that span form.
 min_speak_itemwise <- min_speak_samp %>%
-  left_join(ASD_all, by = c("subjectkey","interview_age","nProduced")) %>% ## Join over itemwise data
+  left_join(ASD_all, by = c("subjectkey","interview_age","nProduced","form")) %>% ## Join over itemwise data
   left_join(select(cdi, word, CDI_ID = num_item_id), by = c("CDI_Metadata_compatible" = "word")) ## Join in CDI codes across forms
 
+## Select just the first administration of CDI.
+first_admins <- min_speak_itemwise %>%
+  group_by(subjectkey) %>%
+  slice_min(interview_age,n = 1)
+
+
+write_rds(min_speak_itemwise, "data/all_minspeak.rds")
+write_rds(first_admins, "data/first_admins_minspeak.rds")
