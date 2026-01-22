5415b42dd187b0eee286c8663923e6c63e87702d
Swest13 <swest19@lsu.edu>
2026-01-21 20:46:16 -0600

    Fix matching between vocab words and CDI POS

---
+++ 003-RandomAcquisitionNetworks.R
@@ -0,0 +1,28 @@
+library(furrr)
+library(future)
+library(purrr)
+library(tidyverse)
+library(igraph)
+library(tibble)
+## RAN functions 
+source("R/Random_Networks.R")
+
+## Individual graphs
+load("data/individual_networks.Rdata")
+cdi <- readRDS("data/cdi-metadata.rds")
+cdi$POS <- ifelse(cdi$lexical_class == "nouns"|cdi$lexical_class=="verbs",cdi$lexical_class, "other")
+
+
+## Tibble with vocab size, graph, and POS
+ran_list <- map(vocab_graphs, function(x){
+  vocab <- names(as_adj_list(x$graph))
+  vocab_size <- length(names(as_adj_list(x$graph)))
+  POS <- data.frame(word = vocab) %>%
+    left_join(select(cdi,CDI_Metadata_compatible,POS), by = c("word" = "CDI_Metadata_compatible")) 
+  POS <- table(POS$POS)
+  
+  return(list(subjectkey = x$id,vocab = vocab,vocab_size = vocab_size, graph = x$graph, POS = POS))                  
+})
+  
+
+data.frame(cue = length(names(as_adj_list(vocab_graphs[[1]]))))
