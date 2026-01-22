526cc44bcff1961296f25459c6d2022e709a85ed
Stanley H West <swest19@lsu.edu>
2026-01-22 15:29:58 -0600

    poly

--- 005-Model_Netstats.R
+++ 005-Model_Netstats.R
@@ -22,14 +22,20 @@ map(split_mods, function(x){
   return(summary(lm(z~nProduced*interview_age*group, data = x)))
 })
 
+## Plot by nProduced
 ggplot(model_df, aes(x = nProduced, y = z,color = group))+
   geom_point() +
   geom_smooth(method = "loess")+
   facet_wrap(~metric)
 
-
+## Plot by interview age
 ggplot(model_df, aes(x = interview_age, y = z,color = group))+
   geom_point() +
   geom_smooth(method = "loess")+
   facet_wrap(~metric)
 
+## Plot nProduced with polynomial regress 
+ggplot(model_df, aes(x = nProduced, y = z,color = group))+
+  geom_point() +
+  geom_smooth(method = "lm", formula = y~poly(x,3))+
+  facet_wrap(~metric)
