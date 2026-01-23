7b921178c0593ec4364418ddf97eca88f89945ea
Stanley H West <swest19@lsu.edu>
2026-01-23 11:10:24 -0600

    Integrate Letti

--- 001-DataSetup.R
+++ 001-DataSetup.R
@@ -7,8 +7,26 @@ load("data/ASD_all_GM.Rdata")
 cdi <- read_rds("data/cdi-metadata.rds")
 
 ## Load Letti data all WG forms
-d_letti <- read.csv("data/LSEL_CDI_new_mod_for_script.csv")
-
+d_letti <- read.csv("data/LSEL_CDI_new_mod_for_script.csv") %>%
+  select(subjectkey = data_id,
+         interview_age = age,
+         sex,
+         Produces = value,
+         CDI_Metadata_compatible = definition) %>%
+  mutate(sex = ifelse(sex == "Male", "M","F"),
+         Produces = ifelse(Produces == "produces", TRUE,FALSE)) %>%
+  group_by(subjectkey,interview_age) %>%
+  mutate(subjectkey_intAge = paste0(subjectkey,"_",interview_age),.after = "subjectkey",
+         nProduced = sum(Produces),
+         form = "WG",
+         group = "yng",
+         CDI_dist = NA,
+         is.threshold = NA,
+         n_entry = 1,
+         year_dist = NA,
+         interview_date = NA,
+         empty = NA,
+         response_code = NA) 
 
 
 ## Making sure WG and WS have the same CDI-compatible lemmas
@@ -17,11 +35,20 @@ sum(unique(ASD_all$CDI_Metadata_compatible[ASD_all$form == "WG"]) %in% unique(AS
 
 ## Merge in Item-wise data and CDI word IDs that span form.
 min_speak_itemwise <- min_speak_samp %>%
-  left_join(ASD_all, by = c("subjectkey","interview_age","nProduced","form")) %>% ## Join over itemwise data
-  left_join(select(cdi, word, CDI_ID = num_item_id), by = c("CDI_Metadata_compatible" = "word")) %>% ## Join in CDI codes across forms
+  left_join(select(ASD_all, -starts_with("word"),-starts_with("class"),-category), by = c("subjectkey","interview_age","nProduced","form")) %>% ## Join over itemwise data
+  left_join(rename(cdi,CDI_ID = num_item_id), by ="CDI_Metadata_compatible" ) %>% ## Join in CDI codes across forms
   filter(nProduced >= 3 ) %>%
   group_by(subjectkey,interview_age) %>%
-  mutate(subjectkey_intAge = paste0(subjectkey,"_",interview_age),.after = "subjectkey")
+  mutate(subjectkey_intAge = paste0(subjectkey,"_",interview_age),.after = "subjectkey",
+         source = "CDI")
+
+d_letti_bind <- d_letti %>%
+  left_join(rename(cdi,CDI_ID = num_item_id), by = "CDI_Metadata_compatible") %>%
+  mutate(num_item_id = CDI_ID) %>%
+  mutate(source = "letti")
+
+combined_df <- rbind(d_letti_bind,min_speak_itemwise)
+
 
 ## Select just the first administration of CDI.
 first_admins <- min_speak_itemwise %>%
@@ -32,3 +59,4 @@ first_admins <- min_speak_itemwise %>%
 
 write_rds(min_speak_itemwise, "data/all_minspeak.rds")
 write_rds(first_admins, "data/first_admins_minspeak.rds")
+write_rds(combined_df, "data/combined_CDILetti.rds")
