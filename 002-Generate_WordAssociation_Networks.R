47a505b0ecbabd2e8cb198561a824474b0bc2165
Stanley H West <swest19@lsu.edu>
2026-02-12 16:25:45 -0600

    TAR stuff

--- 002-Generate_WordAssociation_Networks.R
+++ 002-Generate_WordAssociation_Networks.R
@@ -26,7 +26,8 @@ load("data/associations-child.Rdata")
 vocab_data <- read_rds("data/combined_CDILetti.rds")
 cdi_WG <- read_rds("data/cdi_WG.rds")
 noun_feats <- read.csv("data/MBCDI_concsFeats_2022-07-14.csv")
-
+noun_feats_TRA <- noun_feats %>%
+  filter(Toddler_Access > 4)
 
 ## Resolve inconsistencies between cues/CDI: Association cues in the CoxHae set are slightly different from CDI.
 x <- data.frame(vocab = unique(vocab_data$CDI_Metadata_compatible)) ## Unique Vocab words
@@ -65,9 +66,10 @@ feat_mats <- map(feats_list, ~noun_net(.x))
   
 ## By any feature
 noun_network <- noun_net(noun_feats)
+noun_network_TRA <- noun_net(noun_feats_TRA)
 save(feat_mats, file = "data/feature_graphs.Rdata")
 save(noun_network, file = "data/graph_full_nounNet.Rdata")
-
+save(noun_network_TRA, file = "data/graph_full_nounNet_TRA.Rdata")
 ## Cue-response table for association matrix
 
 cue_resp <- associations_child %>%
@@ -106,9 +108,15 @@ tmp <- noun_feats %>%
   filter(definition %in% names(V(graph_WG))) %>% 
   select(definition) %>% 
   unique()
+tmp_TRA <- noun_feats_TRA %>% 
+  filter(definition %in% names(V(graph_WG))) %>% 
+  select(definition) %>% 
+  unique()
+
 graph_association_nouns_WG <- induced.subgraph(graph = graph_WG, vids = tmp$definition)
 noun_network_WG <- induced.subgraph(noun_network$graph, vids = tmp$definition)
-
+noun_network_WG_TRA <-  induced_subgraph(noun_network_TRA$graph, vids = tmp_TRA$definition)
+save(noun_network_WG_TRA, file = "data/noun_network_WG_TRA.Rdata")
 save(noun_network_WG, file = "data/noun_network_WG.Rdata")
 save(graph_association_nouns, file = "data/association_graph_nouns.Rdata")
 save(graph_association_nouns_WG, file = "data/association_graph_nouns_WG.Rdata")
@@ -171,3 +179,17 @@ ind_noun_graphs_anyfeat <- map(vocab_splits_produced, function(x){
 })
 
 save(ind_noun_graphs_anyfeat,file = "data/ind_noun_graphs_anyfeat.Rdata")
+
+## Individual noun networks any feature
+ind_noun_graphs_anyfeat_TRA <- map(vocab_splits_produced, function(x){
+  x <- x %>%
+    filter(lexical_class == "nouns")
+  ind_adj_mat <- as.matrix(noun_network_TRA$adj_mat[rownames(noun_network_TRA$adj_mat) %in% x$CDI_Metadata_compatible,colnames(noun_network_TRA$adj_mat) %in% x$CDI_Metadata_compatible])
+  return(list(graph = graph_from_adjacency_matrix(ind_adj_mat,mode = "undirected"),form = x$form))
+})
+
+save(ind_noun_graphs_anyfeat_TRA,file = "data/ind_noun_graphs_anyfeat_TRA.Rdata")
+
+
+
+
--- 004-Compute_Standardized_NetStats.R
+++ 004-Compute_Standardized_NetStats.R
@@ -15,6 +15,12 @@ load("data/association_ind_graphs_nouns.Rdata")
 ran_stats_nouns <- read_rds("data/all_RAN_stats_noun.rds")
 load("data/ind_noun_graphs_anyfeat.Rdata")
 
+## Load random stats and vocab networks (noun features TAR filtered)
+ran_stats_nouns_TAR <- read_rds("data/all_RAN_stats_noun_TRA.rds")
+load("data/ind_noun_graphs_anyfeat_TRA.Rdata")
+
+
+
 
 ## Compute true netstats
 ## For associations
@@ -22,12 +28,6 @@ true_stats <- map_dfr(vocab_graphs, function(x){
   network_stats(x$graph)
 }, .id = "subjectkey_intAge")
 
-## For nouns features
-true_stats_N <- map_dfr(ind_noun_graphs_anyfeat, function(x){
-  d <- network_stats(x$graph) 
-  d$nProduced <- vcount(x$graph)
-  return(d)
-}, .id = "subjectkey_intAge")
 
 ## For associations only nouns
 true_stats_assoc_N <-map_dfr(vocab_graphs_association_nouns, function(x){
@@ -36,6 +36,20 @@ true_stats_assoc_N <-map_dfr(vocab_graphs_association_nouns, function(x){
   return(d)
 }, .id = "subjectkey_intAge")
 
+
+## For nouns features
+true_stats_N <- map_dfr(ind_noun_graphs_anyfeat, function(x){
+  d <- network_stats(x$graph) 
+  d$nProduced <- vcount(x$graph)
+  return(d)
+}, .id = "subjectkey_intAge")
+
+## For TAR filtered nouns
+true_stats_N_TAR <- map_dfr(ind_noun_graphs_anyfeat_TRA, function(x){
+  d <- network_stats(x$graph) 
+  d$nProduced <- vcount(x$graph)
+  return(d)
+}, .id = "subjectkey_intAge")
 
 
 z <- function(x){
@@ -71,6 +85,22 @@ all_Netstats_z_N <- true_stats_N %>%
 
 write_rds(all_Netstats_z_N, "data/all_Netstats_z_nouns.rds")
 
+## Noun TAR stats
+all_Netstats_z_N_TAR <- true_stats_N_TAR %>% 
+  left_join(ran_stats_nouns_TAR) %>%
+  pivot_longer(cols = starts_with(c("indegree","clustcoef","meandist")),
+               names_to = c("metric","origin","stat"),
+               values_to = "value",
+               names_sep = "_") %>%
+  mutate(origin = ifelse(is.na(origin), "truenet", origin),
+         stat = ifelse(is.na(stat),"truestat",stat)) %>%
+  group_by(subjectkey_intAge,metric) %>%
+  mutate(z = (value[stat == "truestat"] - value[stat == "mean"])/value[stat == "sd"])
+
+
+write_rds(all_Netstats_z_N_TAR, "data/all_Netstats_z_N_TAR.rds")
+
+
 ## Association noun netstats
 
 all_Netstats_z_assoc_N <- true_stats_assoc_N %>% 
@@ -96,6 +126,10 @@ all_Netstats_z_meta <- all_Netstats_z %>%
 all_Netstats_z_meta_nouns <- all_Netstats_z_N %>%
   left_join(unique(select(vocab, subjectkey_intAge, interview_age, form, group,source)))
 
+all_Netstats_z_meta_nouns_TAR <- all_Netstats_z_N_TAR %>%
+  left_join(unique(select(vocab, subjectkey_intAge, interview_age, form, group,source)))
+
+
 all_Netstats_z_meta_assoc_nouns <- all_Netstats_z_assoc_N %>%
   left_join(unique(select(vocab, subjectkey_intAge, interview_age, form, group,source)))
 
@@ -104,6 +138,7 @@ all_Netstats_z_meta_assoc_nouns <- all_Netstats_z_assoc_N %>%
 saveRDS(all_Netstats_z_meta, "data/all_Netstats_z_meta.rds")
 saveRDS(all_Netstats_z_meta_nouns, "data/all_Netstats_z_meta_nouns.rds")
 saveRDS(all_Netstats_z_meta_assoc_nouns, "data/all_Netstats_z_meta_assoc_nouns.rds")
+saveRDS(all_Netstats_z_meta_nouns_TAR, "data/all_Netstats_z_meta_nouns_TAR.rds")
 
 
 
